dea41cc16d57a3df3fe54f83fb958dfb8e480d70
hkernbach <heiko@arangodb.com>
2020-09-11 13:25:34 +0200

    show more errors

--- react/src/JSONEditor.js
+++ react/src/JSONEditor.js
@@ -176,9 +176,11 @@ const JSONEditor = () => {
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
         console.log(error.request);
+        toast.error(`Error: ${error.request}`);
       } else {
         // Something happened in setting up the request that triggered an Error
         console.log('Error', error.message);
+        toast.error(`Error: ${error.message}`);
       }
       console.log(error.config);
     }
