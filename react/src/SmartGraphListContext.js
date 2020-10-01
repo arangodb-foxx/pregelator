483af46bb4f80a5d74d029ad4edfbd68fbd8a4e1
hkernbach <heiko@arangodb.com>
2020-10-01 16:50:42 +0200

    added jwt

--- react/src/SmartGraphListContext.js
+++ react/src/SmartGraphListContext.js
@@ -1,16 +1,16 @@
 import React, {useState, createContext, useEffect} from 'react';
 import {get} from "axios";
 import {toast} from "react-toastify";
+import jwtControl from "./JWTControl";
 
 export const SmartGraphListContext = createContext();
 
 
 export const SmartGraphListProvider = props => {
-  const [graphs, setGraphs] = useState([
-  ]);
+  const [graphs, setGraphs] = useState([]);
 
   const fetchData = function () {
-    get(process.env.REACT_APP_ARANGODB_COORDINATOR_URL + 'graphs')
+    get(process.env.REACT_APP_ARANGODB_COORDINATOR_URL + 'graphs', jwtControl.getAuthConfig())
       .then((res) => setGraphs(res.data), (error) => {
         toast.error(`This should only occur in dev mode if your foxx app is not deployed, ` + error);
       });
